730bb065d8369295fa422cc6f0d8c32caa03b57c
Max <noelpmax@gmail.com>
2024-10-06 00:02:47 +0200

    migrations sind erledigt, api läuft, templates ordner erstellt

--- portfolio/api/urls.py
+++ portfolio/api/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from .views import guestbook_entries, delete_guestbook_entry
 
 urlpatterns = [
-    path('api/guestbook/', guestbook_entries, name='guestbook_entries'),
-    path('api/guestbook/<int:entry_id>/', delete_guestbook_entry, name='delete_guestbook_entry'),
+    path('guestbook/', guestbook_entries, name='guestbook_entries'),
+    path('guestbook/<int:entry_id>/', delete_guestbook_entry, name='delete_guestbook_entry'),
 ]
